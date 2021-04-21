d5596216f60f06825514f78644e835deeadd3a95
Salina Wu <salinawu1029@gmail.com>
2021-04-21 00:29:46 -0700

    findMedianSortedArrays.py

---
+++ findMedianSortedArrays.py
@@ -0,0 +1,30 @@
+from typing import List
+
+# This works linear time
+# logarithmic time solution: https://medium.com/@hazemu/finding-the-median-of-2-sorted-arrays-in-logarithmic-time-1d3f2ecbeb46
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        i, j = 0, 0
+        whole_list = []
+        while i < len(nums1) or j < len(nums2):
+            if i >= len(nums1):
+                whole_list.extend(nums2[j:])
+                break
+            elif j >= len(nums2):
+                whole_list.extend(nums1[i:])
+                break            
+
+            elif nums1[i] < nums2[j]:
+                whole_list.append(nums1[i])
+                i += 1
+            else:
+                whole_list.append(nums2[j])
+                j += 1
+
+
+        length = len(whole_list)
+        half = floor(length / 2)
+        if length % 2 == 0:
+            return (whole_list[half] + whole_list[half - 1]) / 2
+        else:
+            return whole_list[half]
